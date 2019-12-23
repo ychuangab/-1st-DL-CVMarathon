1ee72f2650f4bd50f250b5c7daea8fc52e117ae1
ychuang <uvxy1234@gmail.com>
2019-12-23 16:30:58 +0800

    upload D21

---
+++ homework/files/utils.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+def dynam_GPU_ram():
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+    config.log_device_placement = True  # to log device placement (on which device the operation ran)
+    sess = tf.compat.v1.Session(config=config)
+    tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=config))
+    
+def GPU_ram_limit(limit=4096):
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=limit)])
